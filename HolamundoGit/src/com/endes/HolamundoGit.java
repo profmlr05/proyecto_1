34a9b57895f9fd50b80ed7a5dd3e0731b67bbda1
profmlr05 <mlazaroaat@gmail.com>
2025-04-01 17:37:55 +0200

    Versión con nueva línea

--- HolamundoGit/src/com/endes/HolamundoGit.java
+++ HolamundoGit/src/com/endes/HolamundoGit.java
@@ -3,5 +3,6 @@ public class HolamundoGit {
 	public static void main (String[] args) {
 		System.out.println("Hola mundo con Java y Git\n");
 		System.out.println("Integración de proyectos en Eclipse y Git para el control de versiones \n");
+		System.out.println("Nueva línea");
 	}
 }
