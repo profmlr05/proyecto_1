2fa69dac219888f03c66cae400fa0a8d1ba3a5bd
profmlr05 <mlazaroaat@gmail.com>
2025-04-01 17:00:40 +0200

    Modificaciones de la clase HolamundoGit

--- HolamundoGit/src/com/endes/HolamundoGit.java
+++ HolamundoGit/src/com/endes/HolamundoGit.java
@@ -1,6 +1,7 @@
 package com.endes;
 public class HolamundoGit {
-	public static void HolamudoGit (String[] args) {
-		System.out.println("Hola mundo con Java y Git");
+	public static void main (String[] args) {
+		System.out.println("Hola mundo con Java y Git\n");
+		System.out.println("Integración de proyectos en Eclipse y Git para el control de versiones \n");
 	}
 }
